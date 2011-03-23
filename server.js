1f8851d4efb4ad7986b208c7d87ae4b2920330b7
hasen j <hasan.aljudy@gmail.com>
2011-03-23 15:25:37 -0600

    Repository for Arabic Web Fonts
    Basic skeleton, font chooser, and font preview pane

---
+++ server.js
@@ -0,0 +1,14 @@
+express = require("express")
+var app = express.createServer();
+app.use(express.static(__dirname + '/data'));
+app.listen(9020);
+
+var fonts = [ ["Kacst Book", "KacstBook.ttf"],
+              ["Kacst One", "KacstOne.ttf"],
+              ["Kacst Pen", "KacstPen.ttf"],
+              ]
+
+app.get('/', function(req, res){
+    res.render('index.jade', {layout: false, fonts: fonts});
+});
+
