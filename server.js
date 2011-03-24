9a25a88557880a67d72ace023ef9916729fb4858
hasen j <hasan.aljudy@gmail.com>
2011-03-24 12:20:08 -0600

    working /css url

--- server.js
+++ server.js
@@ -1,20 +1,39 @@
 express = require("express")
+u = require("underscore")
 var app = express.createServer();
 app.use(express.static(__dirname + '/data'));
 app.set('views', __dirname + '/views');
 app.listen(9020);
 
+var keys = function(obj) { 
+    var acc = [];
+    for(k in obj) { acc.push(k); }
+    return acc; 
+}
+
+
 var fonts = {
-    "Kacst Book": "KacstBook.ttf",
-    "Kacst One": "KacstOne.ttf",
-    "Kacst Pen": "KacstPen.ttf",
+    "KacstBook": "KacstBook.ttf",
+    "KacstOne": "KacstOne.ttf",
+    "KacstPen": "KacstPen.ttf",
     }
 
-fontnames = [];
-for(k in fonts) { fontnames.push(k); }
+fontnames = keys(fonts);
 fontnames.sort();
+
+var fonturl = function(req, name) {
+    var root = "http://" + req.header("host") + "/";
+    return root + fonts[name];
+}
 
 app.get('/', function(req, res){
     res.render('index.jade', {layout: false, fonts: fonts, fontnames: fontnames});
 });
 
+app.get('/css', function(req, res){
+    var names = keys(req.query);
+    u.each(names, function(name) {
+        res.write("@font-face{ \n  font-family:" + name + "; \n  src: url('" + fonturl(req, name) + "'); \n}\n\n");
+    });
+    res.end();
+});
