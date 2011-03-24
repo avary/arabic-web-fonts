48252cd98bc627317a760f71884b92e2b4d70c1f
hasen j <hasan.aljudy@gmail.com>
2011-03-24 13:14:16 -0600

    use the underscore keys function

--- server.js
+++ server.js
@@ -5,20 +5,13 @@ app.use(express.static(__dirname + '/data'));
 app.set('views', __dirname + '/views');
 app.listen(9020);
 
-var keys = function(obj) { 
-    var acc = [];
-    for(k in obj) { acc.push(k); }
-    return acc; 
-}
-
-
 var fonts = {
     "KacstBook": "KacstBook.ttf",
     "KacstOne": "KacstOne.ttf",
     "KacstPen": "KacstPen.ttf",
     }
 
-fontnames = keys(fonts);
+fontnames = u.keys(fonts);
 fontnames.sort();
 
 var fonturl = function(req, name) {
@@ -31,7 +24,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/css', function(req, res){
-    var names = keys(req.query);
+    var names = u.keys(req.query);
     u.each(names, function(name) {
         if(name in fonts) {
             res.write("/*   " + name + "   */ \n");
