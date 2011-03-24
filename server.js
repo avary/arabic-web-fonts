d60b5e8e70947009b6acb41cb691e02cc821a3ca
hasen j <hasan.aljudy@gmail.com>
2011-03-23 22:12:44 -0600

    replace font list with dictionary mapping font name to url

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ app.use(express.static(__dirname + '/data'));
 app.set('views', __dirname + '/views');
 app.listen(9020);
 
-var fonts = [ ["Kacst Book", "KacstBook.ttf"],
-              ["Kacst One", "KacstOne.ttf"],
-              ["Kacst Pen", "KacstPen.ttf"],
-              ]
+var fonts = {
+    "Kacst Book": "KacstBook.ttf",
+    "Kacst One": "KacstOne.ttf",
+    "Kacst Pen": "KacstPen.ttf",
+    }
+
+fontnames = [];
+for(k in fonts) { fontnames.push(k); }
+fontnames.sort();
 
 app.get('/', function(req, res){
-    res.render('index.jade', {layout: false, fonts: fonts});
+    res.render('index.jade', {layout: false, fonts: fonts, fontnames: fontnames});
 });
 
