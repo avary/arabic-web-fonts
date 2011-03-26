550a86f511fa9aaf39cc47352a8e0d1105a38ec7
hasen j <hasan.aljudy@gmail.com>
2011-03-26 10:13:04 -0600

    Change font url and allow access control for everyone
    Embedding on other domains should now work!

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 express = require("express")
 u = require("underscore")
 var app = express.createServer();
-app.use(express.static(__dirname + '/data'));
+// app.use(express.static(__dirname + '/data'));
 app.set('views', __dirname + '/views');
 app.listen(9020);
 
@@ -15,17 +15,26 @@ var fonts = {
 fontnames = u.keys(fonts);
 fontnames.sort();
 
+var font_exists = function(name) { return name in fonts; }
+
 var fonturl = function(req, name) {
-    var root = "http://" + req.header("host") + "/";
-    return root + fonts[name];
+    return "http://" + req.header("host") + "/font/" + name;
 }
 
 app.get('/', function(req, res){
     res.render('index.jade', {layout: false, fonts: fonts, fontnames: fontnames});
 });
 
+app.get('/font/:name', function(req, res) {
+    if(!font_exists(req.params.name)) { res.send("Font doesn't exists: " + req.params.name, 404); }
+    else {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.download(__dirname + "/fonts/" + fonts[req.params.name]);
+    }
+});
+
 app.get('/css', function(req, res){
-    res.writeHead(200, { 'Content-type': 'text/css' });
+    res.contentType("text/css");
     var names = u.keys(req.query);
     u.each(names, function(name) {
         if(name in fonts) {
